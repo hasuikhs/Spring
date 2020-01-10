d8d8683d65b3a44ea1a675dae99f794cc31e1f06
Hanseok Kim <hasuikhs@gmail.com>
2020-01-10 18:00:57 +0900

    200110|add pr

---
+++ pr/src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,24 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+	
+	private int pageNum;
+	private int amount;
+	
+	public Criteria() {
+		// 기본값을 1페이지 10개로 지정
+		this(1, 10);
+	}
+	
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+}
---
+++ pr/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,25 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+public interface BoardMapper {
+
+	// @Select("SELECT * FROM tbl_board WHERE bno >0")
+	public List<BoardVO> getList();
+
+	// 페이징
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+}
